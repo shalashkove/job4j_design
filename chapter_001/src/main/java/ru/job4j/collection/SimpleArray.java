7b9004d633bc5be4b582976b999689a440129b70
shalashkove <shalashkove@gmail.com>
2020-09-17 19:10:30 +0500

    1. Динамический список на массиве.[158#297481]

---
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleArray.java
@@ -0,0 +1,76 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class SimpleArray<T> implements Iterable<T> {
+    private final static int DEFAULT_CAPACITY = 10;
+    private int modCount;
+    private int size;
+    private int capacity;
+    private Object[] container;
+
+    public SimpleArray() {
+        this.modCount = 0;
+        this.size = 0;
+        this.capacity = DEFAULT_CAPACITY;
+        this.container = new Object[DEFAULT_CAPACITY];
+    }
+
+//    public SimpleArray(int capacity) {
+//        this.modCount = 0;
+//        this.size = 0;
+//        if (capacity >= 0) {
+//            this.capacity = capacity;
+//        } else {
+//            throw new IllegalArgumentException();
+//        }
+//        this.container = new Object[capacity];
+//    }
+
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        return (T) container[index];
+    }
+
+    public void add(T model) {
+        modCount++;
+        if (size == capacity) {
+            grow();
+        }
+        container[size++] = model;
+    }
+
+    private void grow() {
+        capacity = capacity * 2;
+        container = Arrays.copyOf(container, capacity);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Itr();
+    }
+
+    private class Itr implements Iterator<T> {
+        int cursor;
+        int lastRet = -1;
+        int expectedModCount = modCount;
+
+        @Override
+        public boolean hasNext() {
+            return cursor != size;
+        }
+
+        @Override
+        public T next() {
+            if (modCount != expectedModCount) {
+                throw new ConcurrentModificationException();
+            }
+            if (cursor >= size) {
+                throw new NoSuchElementException();
+            }
+            lastRet = cursor;
+            cursor++;
+            return (T) container[lastRet];
+        }
+    }
+}

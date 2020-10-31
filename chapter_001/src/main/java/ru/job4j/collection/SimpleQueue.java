49e0ba2b07cd8c5054b0f82e117a53e35ecb58db
shalashkove <shalashkove@gmail.com>
2020-10-31 10:50:26 +0500

    5. Очередь на двух стеках[160#297483]

--- chapter_001/src/main/java/ru/job4j/collection/SimpleQueue.java
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -7,21 +7,12 @@ public class SimpleQueue<T> {
 
     public T poll() {
         T result = null;
-//        while (!in.isEmpty()) {
-//                out.push(in.pop());
-//        }
-//        result = out.pop();
-//        while (!out.isEmpty()) {
-//                in.push(out.pop());
-//        }
         if (out.isEmpty()) {
-        while (!in.isEmpty()) {
+            while (!in.isEmpty()) {
                 out.push(in.pop());
+            }
         }
         result = out.pop();
-        } else {
-            result = out.pop();
-        }
         return result;
     }
 

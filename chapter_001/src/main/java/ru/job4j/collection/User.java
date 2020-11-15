21f3e53b89f78f6c3234801f66d187f3f076683a
shalashkove <shalashkove@gmail.com>
2020-11-15 11:35:51 +0500

    2. Не перекрывать equals hashCode[1005#297513]

--- chapter_001/src/main/java/ru/job4j/collection/User.java
+++ chapter_001/src/main/java/ru/job4j/collection/User.java
@@ -1,6 +1,9 @@
 package ru.job4j.collection;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     private String name;
@@ -14,5 +17,14 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        User user1 = new User("name1", 1, new GregorianCalendar(1975, Calendar.JANUARY, 1));
+        User user2 = new User("name1", 1, new GregorianCalendar(1975, Calendar.JANUARY, 1));
+        Map<User, Object> mapa = Map.of(
+                user1, new Object(),
+                user2, new Object());
+        System.out.println(mapa);
     }
 }
--- chapter_001/src/test/java/ru/job4j/collection/UserTest.java
+++ chapter_001/src/test/java/ru/job4j/collection/UserTest.java
@@ -1,8 +1,6 @@
 package ru.job4j.collection;
 
-import java.util.Calendar;
 
 public class UserTest {
-
 
 }

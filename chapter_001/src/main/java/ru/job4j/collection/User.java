f345e2350874bdfac61774584ff3493271b70dd9
shalashkove <shalashkove@gmail.com>
2020-11-17 19:32:15 +0500

    3. Переопределить только hashCode[1003#297511]

--- chapter_001/src/main/java/ru/job4j/collection/User.java
+++ chapter_001/src/main/java/ru/job4j/collection/User.java
@@ -1,9 +1,6 @@
 package ru.job4j.collection;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class User {
     private String name;
@@ -19,12 +16,30 @@ public class User {
         this.birthday = birthday;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return children == user.children &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(birthday, user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+
     public static void main(String[] args) {
         User user1 = new User("name1", 1, new GregorianCalendar(1975, Calendar.JANUARY, 1));
         User user2 = new User("name1", 1, new GregorianCalendar(1975, Calendar.JANUARY, 1));
         Map<User, Object> mapa = Map.of(
                 user1, new Object(),
                 user2, new Object());
+//        Map<User, Object> mapa = new HashMap<>();
+//        mapa.put(user1, new Object());
+//        mapa.put(user2, new Object());
         System.out.println(mapa);
     }
 }

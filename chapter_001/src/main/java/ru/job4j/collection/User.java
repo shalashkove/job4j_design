27c1e53a61720a66990a0aba7554a1edb6fe21a0
shalashkove <shalashkove@gmail.com>
2020-11-20 18:59:58 +0500

    5. Перекрывать и equals и hashCode[1002#297510]

--- chapter_001/src/main/java/ru/job4j/collection/User.java
+++ chapter_001/src/main/java/ru/job4j/collection/User.java
@@ -30,17 +30,21 @@ public class User {
                 && Objects.equals(birthday, user.birthday);
     }
 
-//    @Override
-//    public int hashCode() {
-//        return ((31 + name.hashCode() * 31) * 31 + children * 31) * 31 + birthday.hashCode() * 31;
-//    }
+    @Override
+    public int hashCode() {
+        return ((31 + name.hashCode() * 31) * 31 + children * 31) * 31 + birthday.hashCode() * 31;
+    }
 
     public static void main(String[] args) {
         User user1 = new User("name1", 1, new GregorianCalendar(1975, Calendar.JANUARY, 1));
         User user2 = new User("name1", 1, new GregorianCalendar(1975, Calendar.JANUARY, 1));
+        Object object1 = new Object();
+        Object object2 = new Object();
+        System.out.println("user1 = " + user1 + "   Object1 = " + object1);
+        System.out.println("user2 = " + user2 + "   Object2 = " + object2);
         Map<User, Object> mapa = Map.of(
-                user1, new Object(),
-                user2, new Object());
+                user1, object1,
+                user2, object2);
         System.out.println(mapa);
     }
 }

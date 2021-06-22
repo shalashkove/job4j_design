f9a07cb34c88311d905b8e95fd0407d1a194073e
shalashkove <shalashkove@gmail.com>
2021-06-22 13:06:30 +0500

    4. Slf4j - вывод exception. [#268853]

--- chapter_001/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_001/src/main/java/ru/job4j/io/EchoServer.java
@@ -3,40 +3,48 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class EchoServer {
-    public static void main(String[] args) throws IOException {
-        try (ServerSocket server = new ServerSocket(9000)) {
-            while (!server.isClosed()) {
-                Socket socket = server.accept();
-                try (OutputStream out = socket.getOutputStream();
-                     BufferedReader in = new BufferedReader(
-                             new InputStreamReader(socket.getInputStream()))) {
-                    boolean isExit = false;
-                    String msg = null;
-                    for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        System.out.println(str);
-                        if (str.contains("?msg=Exit")) {
-                            isExit = true;
-                            continue;
-                        }
-                        if (str.contains("?msg=")) {
-                            String[] arr = str.split("=");
-                            if (arr.length == 2) {
-                                msg = arr[1].split(" ")[0];
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+
+    public static void main(String[] args) {
+        try {
+            try (ServerSocket server = new ServerSocket(9000)) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream();
+                         BufferedReader in = new BufferedReader(
+                                 new InputStreamReader(socket.getInputStream()))) {
+                        boolean isExit = false;
+                        String msg = null;
+                        for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
+                            System.out.println(str);
+                            if (str.contains("?msg=Exit")) {
+                                isExit = true;
+                                continue;
+                            }
+                            if (str.contains("?msg=")) {
+                                String[] arr = str.split("=");
+                                if (arr.length == 2) {
+                                    msg = arr[1].split(" ")[0];
+                                }
                             }
                         }
-                    }
-                    if (msg != null && !isExit) {
-                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                        out.write(msg.getBytes());
-                    }
-                    if (isExit) {
-                        out.write("HTTP/1.1 200 OK\r\n".getBytes());
-                        server.close();
+                        if (msg != null && !isExit) {
+                            out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                            out.write(msg.getBytes());
+                        }
+                        if (isExit) {
+                            out.write("HTTP/1.1 200 OK\r\n".getBytes());
+                            server.close();
+                        }
                     }
                 }
             }
+        } catch (Exception e) {
+            LOG.error("Exception in main method", e);
         }
     }
 }

2b3f2acad6abf2ab5b1ee0df655c7f32c49a8502
shalashkove <shalashkove@gmail.com>
2021-12-01 22:11:17 +0500

    7. Scanner [#504791]
    7. Scanner [#504791]

--- chapter_001/src/main/java/ru/job4j/io/CSVReader.java
+++ chapter_001/src/main/java/ru/job4j/io/CSVReader.java
@@ -23,49 +23,30 @@ public class CSVReader {
     }
 
     public static void handle(ArgsName argsName) {
-        try {
-            Scanner scanner = new Scanner(new FileInputStream(new File(argsName.get("path"))));
+        final String DELIMITER = argsName.get("delimiter");
+        try (Scanner scanner = new Scanner(new FileInputStream(new File(argsName.get("path"))))) {
             String[] columnsList = null;
+            String headersString = null;
             if (scanner.hasNextLine()) {
-                columnsList = scanner.nextLine().split(";");
+                headersString = scanner.nextLine();
+                columnsList = headersString.split(DELIMITER);
             }
-            scanner.close();
             String[] setColumns = argsName.get("filter").split(",");
             List<Integer> columnsToOut = CSVReader.columnNumberDefine(columnsList, setColumns);
-            Pattern pattern = Pattern.compile("([\\n" + argsName.get("delimiter") + "]|(\\r\\n))+");
-            scanner = new Scanner(new FileInputStream(new File(argsName.get("path"))))
-                    .useDelimiter(pattern);
-            int cellInRaw = 0;
-            int cursor = 0;
             StringBuilder outData = new StringBuilder();
-            while (scanner.hasNext()) {
-                String cell = scanner.next();
-                for (int el : columnsToOut) {
-                    if (el == cursor) {
-                        outData.append(cell);
-                        cellInRaw++;
-                        if (cellInRaw == setColumns.length || cursor == columnsList.length) {
-                            outData.append(System.lineSeparator());
-                        } else {
-                            outData.append(argsName.get("delimiter"));
-                        }
-                        break;
-                    }
-                }
-                cursor++;
-                if (cursor == columnsList.length) {
-                    cursor = 0;
-                }
-                if (cellInRaw == setColumns.length) {
-                    cellInRaw = 0;
-                }
+            outData.append(CSVReader.formString(DELIMITER, headersString, columnsToOut));
+            while (scanner.hasNextLine()) {
+                String newLine = scanner.nextLine();
+                outData.append(CSVReader.formString(DELIMITER, newLine, columnsToOut));
             }
             if ("stdout".equals(argsName.get("out"))) {
                 System.out.println(outData.toString());
-            } else  {
+            } else {
                 File file = new File(argsName.get("out"));
                 try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(file))) {
                     out.write(outData.toString().getBytes());
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
         } catch (Exception e) {
@@ -73,7 +54,20 @@ public class CSVReader {
         }
     }
 
-    public static List<Integer> columnNumberDefine(String[] columnsList, String[] setColumns) {
+    public static StringBuilder formString(String delimiter, String inString, List<Integer> columnsToOut) {
+        StringBuilder sb = new StringBuilder();
+        String[] rowSting = inString.split(delimiter);
+        for (int i = 0; i != columnsToOut.size(); i++) {
+            sb.append(rowSting[columnsToOut.get(i)]);
+            if (i != columnsToOut.size() - 1) {
+                sb.append(delimiter);
+            }
+        }
+        sb.append(System.lineSeparator());
+        return sb;
+    }
+
+    public static List<Integer> columnNumberDefine(String[]columnsList, String[]setColumns) {
         List<Integer> result = new ArrayList<>();
         for (String el : setColumns) {
             for (int i = 0; i != columnsList.length; i++) {
@@ -85,7 +79,7 @@ public class CSVReader {
         return result;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[]args) {
         ArgsName argsName = ArgsName.of(args);
         CSVReader csvReader = new CSVReader();
         if (csvReader.validation(argsName)) {
--- chapter_001/src/test/java/ru/job4j/io/CSVReaderTest.java
+++ chapter_001/src/test/java/ru/job4j/io/CSVReaderTest.java
@@ -65,4 +65,31 @@ public class CSVReaderTest {
         Assert.assertEquals(expected, Files.readString(target.toPath()));
     }
 
+    @Test
+    public void whenFilterAllColumns() throws Exception {
+        String data = String.join(
+                System.lineSeparator(),
+                "name;age;last_name;education",
+                "Tom;20;Smith;Bachelor",
+                "Jack;25;Johnson;Undergraduate",
+                "William;30;Brown;Secondary special"
+        );
+        File file = temporaryFolder.newFile("source.csv");
+        File target = temporaryFolder.newFile("target.csv");
+        ArgsName argsName = ArgsName.of(new String[]{
+                "-path=" + file.getAbsolutePath(), "-delimiter=;", "-out="
+                + target.getAbsolutePath(), "-filter=age,name,last_name,education"
+        });
+        Files.writeString(file.toPath(), data);
+        String expected = String.join(
+                System.lineSeparator(),
+                "age;name;last_name;education",
+                "20;Tom;Smith;Bachelor",
+                "25;Jack;Johnson;Undergraduate",
+                "30;William;Brown;Secondary special"
+        ).concat(System.lineSeparator());
+        CSVReader.handle(argsName);
+        Assert.assertEquals(expected, Files.readString(target.toPath()));
+    }
+
 }

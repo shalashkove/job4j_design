b4dd3137f19f5b878e6f37302099751928ddcfa2
shalashkove <shalashkove@gmail.com>
2022-05-25 13:02:54 +0500

    2. Группировка и агрегатные функции [#373618]

---
+++ chapter_001/data/script_07_(group).sql
@@ -0,0 +1,40 @@
+create table devices(
+    id serial primary key,
+    name varchar(255),
+    price float
+);
+
+create table people(
+    id serial primary key,
+    name varchar(255)
+);
+
+create table devices_people(
+    id serial primary key,
+    device_id int references devices(id),
+    people_id int references people(id)
+);
+
+insert into devices(name, price) values ('Телефон', 12000.00), ('Наушники', 500.50), ('Гарнитура', 2500.40);
+insert into people(name) values ('Вася'), ('Петя'), ('Миша');
+insert into devices_people(device_id, people_id) values (1, 1), (2, 1), (3, 1);
+insert into devices_people(device_id, people_id) values (1, 2), (2, 2);
+insert into devices_people(device_id, people_id) values (2, 3);
+
+select avg(price) from devices;
+
+select tab.name, avg(d.price)
+from (select * from devices_people join people
+on devices_people.people_id = people.id) as tab
+join devices d
+on tab.device_id = d.id
+group by tab.name;
+
+select tab.name, avg(d.price)
+from (select * from devices_people join people
+on devices_people.people_id = people.id) as tab
+join devices d
+on tab.device_id = d.id
+group by tab.name
+having avg(d.price) > 5000;
+
